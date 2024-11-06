507baa811cae5c2b35e3e26f82b50da6ab420554
shinminkyoung1 <dottiee99@naver.com>
2024-11-06 23:16:02 +0900

    feat: mock 데이터 기본 레포지터리 및 서비스 구현

---
+++ src/main/java/salary_BE/salary/Repository/UserRepository.java
@@ -0,0 +1,10 @@
+package salary_BE.salary.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import salary_BE.salary.Domain.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findById(Long user_id);
+}
---
+++ src/main/java/salary_BE/salary/Repository/WordRepository.java
@@ -0,0 +1,2 @@
+package salary_BE.salary.Repository;public interface WordRepository {
+}
