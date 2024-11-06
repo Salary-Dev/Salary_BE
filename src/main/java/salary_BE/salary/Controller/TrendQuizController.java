76e3ae8cfca56f39067af86e017dabb5144485dc
shinminkyoung1 <dottiee99@naver.com>
2024-11-06 16:19:36 +0900

    feat: 트렌드 퀴즈 요청

--- src/main/java/salary_BE/salary/Controller/TrendQuizController.java
+++ src/main/java/salary_BE/salary/Controller/TrendQuizController.java
@@ -1,2 +1,23 @@
-package salary_BE.salary.Controller;public class TrendQuizController {
+package salary_BE.salary.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import salary_BE.salary.Service.TrendQuizService;
+
+@RestController
+public class TrendQuizController {
+
+    private final TrendQuizService trendQuizService;
+
+    @Autowired
+    public TrendQuizController(TrendQuizService trendQuizService) {
+        this.trendQuizService = trendQuizService;
+    }
+
+    @GetMapping("/trend-quiz")
+    public String getTrendQuiz() {
+        // 반환 JSON 응답 그대로 반환
+        return trendQuizService.getTrendQuiz();
+    }
 }
