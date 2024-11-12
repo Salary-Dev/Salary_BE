7ff0c7aeecdb8b09e3883dfbd292c39599d7300b
shinminkyoung1 <dottiee99@naver.com>
2024-11-13 00:07:37 +0900

    refactor: 유저 다중 데이터 처리

--- src/main/java/salary_BE/salary/Repository/AttendanceRepository.java
+++ src/main/java/salary_BE/salary/Repository/AttendanceRepository.java
@@ -1,15 +1,18 @@
 package salary_BE.salary.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.thymeleaf.templateparser.text.AbstractTextTemplateParser;
 import salary_BE.salary.Domain.Attendance;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
 
     Attendance findByUserIdAndAttendanceDate(Long user_id, LocalDate attendance_date);
-    Optional<Attendance> findByUserId(Long user_id);
+    List<Attendance> findAllByUserId(Long userId);
 
 }
--- src/main/java/salary_BE/salary/Service/AttendanceService.java
+++ src/main/java/salary_BE/salary/Service/AttendanceService.java
@@ -11,6 +11,7 @@ import salary_BE.salary.Repository.WordRepository;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -38,16 +39,20 @@ public class AttendanceService {
     public Long getTodayWord() {
         User currentUser = userService.getCurrentUser(); // 현재 사용자 조회
 
-        // 출석 정보 가져오기
-        Attendance attendance = attendanceRepository.findByUserId(currentUser.getId())
-                .orElseThrow(() -> new RuntimeException("출석 정보가 없습니다."));
+        // current User의 userId와 매칭되는 Attendance 데이터 가져오기
+        List<Attendance> userAttendances = attendanceRepository.findAllByUserId(currentUser.getId());
 
-        // 마지막 학습 단어 확인
-        Long lastWordId = attendance.getLastWordId();
+        if (userAttendances.isEmpty()) {
+            throw new RuntimeException("출석 정보가 없습니다.");
+        }
 
-        // 다음 학습 단어 ID 반환
-        return wordRepository.findById(lastWordId + 1)
-                .orElseThrow(() -> new RuntimeException("모든 단어를 학습했습니다."))
-                .getId();
+        // 가장 큰 last_word_Id 찾기
+        Long maxLastWordId = userAttendances.stream()
+                .map(Attendance::getLastWordId)
+                .max(Long::compareTo)
+                .orElseThrow(() -> new RuntimeException("출석 정보에서 마지막 학습 단어 ID를 찾을 수 없습니다."));
+
+        // 다음 학습할 단어 ID 반환
+        return maxLastWordId + 1;
     }
 }
