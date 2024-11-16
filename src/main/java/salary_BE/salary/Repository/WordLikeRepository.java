d1a4bb55fa4d78f8bc6c03292c0c35627dd9752f
SHIN__0.0 <ajtwoddltom@gmail.com>
2024-11-16 16:52:13 +0900

    refactor/버그수정 단어개수 12개제한

--- src/main/java/salary_BE/salary/Repository/WordLikeRepository.java
+++ src/main/java/salary_BE/salary/Repository/WordLikeRepository.java
@@ -21,7 +21,7 @@ public interface WordLikeRepository extends JpaRepository<WordLike, Long> {
 
     Optional<WordLike> findTopByUserOrderByLikeDateDesc(User user);
 
-    @Query("SELECT wl FROM WordLike wl ORDER BY function('RAND')")
+    @Query(value = "SELECT * FROM word_like ORDER BY RAND() LIMIT 12", nativeQuery = true)
     List<WordLike> findRandomWordLikesLimit12(); //무작위로 12개 북마킹 단어 선정
 
     // 특정 단어를 북마크했는지 확인
