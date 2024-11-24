3ab127f7421905774d577e6230a259203257c172
shinminkyoung1 <dottiee99@naver.com>
2024-11-24 17:24:59 +0900

    feat: 시드 조회 기본 로직 구현

--- src/main/java/salary_BE/salary/Controller/SeedController.java
+++ src/main/java/salary_BE/salary/Controller/SeedController.java
@@ -2,9 +2,9 @@ package salary_BE.salary.Controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import salary_BE.salary.Domain.Attendance;
+import salary_BE.salary.Service.AttendanceService;
 import salary_BE.salary.Service.SeedService;
 
 import java.util.HashMap;
@@ -15,6 +15,7 @@ import java.util.Map;
 public class SeedController {
 
     private final SeedService seedService;
+    private final AttendanceService attendanceService;
 
     // 시드 변경
     @PatchMapping("/seed/update")
@@ -27,6 +28,21 @@ public class SeedController {
 
         Map<String, String> response = new HashMap<>();
         response.put("status", "success");
+        return ResponseEntity.ok(response);
+    }
+
+    // 시드 조회
+    @GetMapping("/seed")
+    public ResponseEntity<Map<String, Object>> getSeedLog(@RequestParam("date") String attendanceDate) {
+        Attendance attendance = attendanceService.getAttendanceByDate(attendanceDate); // 기존 구현된 함수 이용
+
+        // 조회된 시드 로그를 JSON 형태로 변환
+        Map<String, Object> response = new HashMap<>();
+        response.put("attendance_date", attendance.getAttendanceDate());
+        response.put("today_seed", attendance.getTodaySalaryPoint());
+        response.put("today_seed_earned", attendance.getTodaySalaryPoint_earned());
+        response.put("today_seed_used", attendance.getTodaySalaryPoint_used());
+
         return ResponseEntity.ok(response);
     }
 }
