f2278f5f02683e3866b7faec6651e58103a23d64
SHIN__0.0 <ajtwoddltom@gmail.com>
2024-11-27 02:25:41 +0900

    refactor/영속성 문제해결(wordService)

--- src/main/java/salary_BE/salary/Service/WordService.java
+++ src/main/java/salary_BE/salary/Service/WordService.java
@@ -1,5 +1,6 @@
 package salary_BE.salary.Service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import salary_BE.salary.DTO.ArticleDto;
@@ -7,6 +8,7 @@ import salary_BE.salary.DTO.WordmainDto;
 import salary_BE.salary.Domain.ArticleWordMapping;
 import salary_BE.salary.Domain.User;
 import salary_BE.salary.Domain.Word;
+import salary_BE.salary.Repository.UserRepository;
 import salary_BE.salary.Repository.WordLikeRepository;
 import salary_BE.salary.Repository.WordRepository;
 import salary_BE.salary.Repository.ArticleWordMappingRepository;
@@ -17,12 +19,14 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class WordService {
 
     private final WordRepository wordRepository;
     private final ArticleWordMappingRepository articleWordMappingRepository;
     private final WordLikeRepository wordLikeRepository;
     private final UserService userService;
+    private final UserRepository userRepository;
 
     public Optional<WordmainDto> getWordById(Long wordId, User user) {
         Optional<Word> word = wordRepository.findById(wordId);
@@ -38,8 +42,9 @@ public class WordService {
                     ))
                     .collect(Collectors.toList());
 
+
             // 현재 사용자가 이 단어를 북마크했는지 확인
-            boolean isSavedByUser = wordLikeRepository.existsByUserAndWordAndWordBookmarkTrue(user, wordEntity);
+            boolean isSavedByUser = wordLikeRepository.existsByUserIdAndWordAndWordBookmarkTrue(user.getId(), wordEntity);
             return Optional.of(new WordmainDto(
                     wordEntity.getId(),
                     wordEntity.getWord(),
@@ -68,7 +73,7 @@ public class WordService {
                     ))
                     .collect(Collectors.toList());
 
-            boolean isSavedByUser = wordLikeRepository.existsByUserAndWordAndWordBookmarkTrue(user, wordEntity);
+            boolean isSavedByUser = wordLikeRepository.existsByUserIdAndWordAndWordBookmarkTrue(user.getId(), wordEntity);
             return Optional.of(new WordmainDto(
                     wordEntity.getId(),
                     wordEntity.getWord(),
@@ -99,7 +104,7 @@ public class WordService {
                             .collect(Collectors.toList());
 
                     // 현재 사용자가 이 단어를 북마크했는지 확인
-                    boolean isSavedByUser = wordLikeRepository.existsByUserAndWordAndWordBookmarkTrue(user, word);
+                    boolean isSavedByUser = wordLikeRepository.existsByUserIdAndWordAndWordBookmarkTrue(user.getId(), word);
 
                     return new WordmainDto(
                             word.getId(),
@@ -132,7 +137,7 @@ public class WordService {
                             .collect(Collectors.toList());
 
                     // 현재 사용자가 이 단어를 북마크했는지 확인
-                    boolean isSavedByUser = wordLikeRepository.existsByUserAndWordAndWordBookmarkTrue(user, word);
+                    boolean isSavedByUser = wordLikeRepository.existsByUserIdAndWordAndWordBookmarkTrue(user.getId(), word);
 
                     return new WordmainDto(
                             word.getId(),
